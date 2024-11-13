4794c3d0543f2242d8c6a72b3f999d2fa579c974
Maruf Ahmed <109242141+maufdev09@users.noreply.github.com>
2024-11-13 08:51:50 +0600

    Initial commit

---
+++ src/middleware/notFound.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction,  } from 'express';
+
+const notFound = (req: Request, res: Response, next: NextFunction) => {
+  return res.status(404).json({
+    success: false,
+    message: "API Not Found",
+    error: "",
+  });
+};
+
+export default notFound;
